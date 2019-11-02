4bc7464096b34ce20b7013178025c773569924d7
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-02 17:51:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,9 +9,14 @@ class Shoe
     @brand 
   end 
   
+<<<<<<< HEAD
   def cobble
     @condition = "new"
     puts "Your shoe is as good as new!"
   end
 
+=======
+  def cobble 
+end
+>>>>>>> faa1315348c1e88352ac15e4854c62a80b65aa56
 end 
